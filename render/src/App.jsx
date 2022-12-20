a1f4f0e4134dec298b3ffb292dc256f299751c39
Ashimbekow <Ashimbekoverbolot@gmail.com>
2022-12-20 15:48:02 +0330

    DZ4

--- render/src/App.jsx
+++ render/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
  const [passwordDirty, setPasswordDirty] = useState(false)
  const [emailError, setEmailError] = useState("Пожалуйста заполните! ")
  const [passwordError, setPasswordError] = useState("Заполните!")
-
+const [check, setCheck] = useState(false)
 
 
  const blurHandler =(e) =>{
@@ -28,10 +28,11 @@ function App() {
      <form>
       <h1>Регистрация</h1>
       {(emailDirty && emailError) && <div style={{color:"red"}}>{emailError}</div> }
-      <input onBlur={e => blurHandler(e)} name='email' type="text" placeholder='Введите свой email...' />
+      <input onBlur={e => blurHandler(e)} name='email' type="text" placeholder='Введите свой email...' disabled={check} />
       {(passwordDirty && passwordError) && <div style={{color:"red"}}>{passwordError}</div> }
-      <input onBlur={e => blurHandler(e)} name='password' type="password" placeholder='Введите свой password'/>
-      <button type='submit'>Зарегистрироваться!</button>
+      <input onBlur={e => blurHandler(e)} name='password' type="password" placeholder='Введите свой password' disabled={check}/>
+      <input type="checkbox" value={check} onChange={() => setCheck(!check)} />
+      <button  type='submit' disabled={!check}>Зарегистрироваться!</button>
      </form>
     </div>
    
