4a38896f3f29743808b50b7ef52a1f2b90662f0d
Ashimbekow <Ashimbekoverbolot@gmail.com>
2022-12-20 14:10:30 +0330

    set

---
+++ render/src/App.jsx
@@ -0,0 +1,41 @@
+
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+ const [email,setEmail] = useState("")
+ const [password,setPassword] = useState("")
+ const [emailDirty, setEmailDirty] = useState(false)
+ const [passwordDirty, setPasswordDirty] = useState(false)
+ const [emailError, setEmailError] = useState("Пожалуйста заполните! ")
+ const [passwordError, setPasswordError] = useState("Заполните!")
+
+
+
+ const blurHandler =(e) =>{
+  switch(e.target.name){
+    case "email":
+      setEmailDirty (true)
+      break
+      case "password":
+        setPasswordDirty (true)
+      break
+  }
+ }
+
+  return (
+    <div className="App">
+     <form>
+      <h1>Регистрация</h1>
+      {(emailDirty && emailError) && <div style={{color:"red"}}>{emailError}</div> }
+      <input onBlur={e => blurHandler(e)} name='email' type="text" placeholder='Введите свой email...' />
+      {(passwordDirty && passwordError) && <div style={{color:"red"}}>{passwordError}</div> }
+      <input onBlur={e => blurHandler(e)} name='password' type="password" placeholder='Введите свой password'/>
+      <button type='submit'>Зарегистрироваться!</button>
+     </form>
+    </div>
+   
+  )
+}
+
+export default App
